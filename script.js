a4205186d5b1a085713db6639de03ffb9c14d2b9
Nag1 <13264500190@163.com>
2023-02-26 20:02:04 +0800

    add bot script

--- script.js
+++ script.js
@@ -38,32 +38,28 @@ const init = function () {
 init();
 
 const switchPlayer = function () {
+  btnRoll.classList.toggle('hidden');
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
   currentScore = 0;
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
-// document.querySelector(".dice") =
 
 //Rolling dice functionality
 
 btnRoll.addEventListener('click', function () {
   if (playing) {
-    //1.Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-    //2.Display dice
     diceEl.classList.remove('hidden');
     diceEl.src = `images/dice-${dice}.png`;
-    //3.Check for rolled 1:if true, switch to next player
     if (dice !== 1) {
-      //Add dice to current score
       currentScore += dice;
       document.getElementById(`current--${activePlayer}`).textContent =
         currentScore;
     } else {
-      //Switch to next player
       switchPlayer();
+      setTimeout(aiTurn, 2000);
     }
   }
 });
@@ -73,7 +69,6 @@ btnHold.addEventListener('click', function () {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-
     if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
@@ -81,15 +76,12 @@ btnHold.addEventListener('click', function () {
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
       document
-        .querySelector(`.layer--${activePlayer}`)
+        .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
     } else {
       switchPlayer();
+      setTimeout(aiTurn, 1000);
     }
-    //1.Add current score to active player's score
-
-    //2.Check if player's score is>=100
-    //Finish the game
   }
 });
 
@@ -97,3 +89,31 @@ btnNew.addEventListener('click', function () {
   init();
   switchPlayer();
 });
+
+const aiTurn = function () {
+  if (activePlayer === 1 && playing) {
+    btnRoll.classList.add('hidden');
+    let dice = Math.trunc(Math.random() * 6) + 1;
+    diceEl.classList.remove('hidden');
+    diceEl.src = `images/dice-${dice}.png`;
+    if (dice !== 1) {
+      currentScore += dice;
+      let dice2 = Math.trunc(Math.random() * 6) + 1;
+      diceEl.src = `images/dice-${dice}.png`;
+      if (dice2 !== 1) {
+        currentScore += dice2;
+        document.getElementById(`current--${activePlayer}`).textContent =
+          currentScore;
+      }
+
+      if (currentScore + scores[activePlayer] >= 100) {
+        btnHold.click();
+      } else {
+        setTimeout(aiTurn, 1000);
+        btnHold.click();
+      }
+    } else {
+      switchPlayer();
+    }
+  }
+};
